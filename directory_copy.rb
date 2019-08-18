2e495e4c6e782b764a513cdd49855681750d6a38
Elsie Ashworth <44971680+eashworth@users.noreply.github.com>
2019-08-18 17:49:22 +0100

    Exercises 5 & 7: add cohort, country, hobby, height; check name for typos

--- directory_copy.rb
+++ directory_copy.rb
@@ -2,27 +2,60 @@
 # 1. Added each_with_index to print method, to print number before name.
 # 2. Print only the students whose name begins with a specific letter.
 # 3. Print only the students whose name is shorter than 12 characters.
-# Rewrite the each() method that prints all students using while or until control flow methods (Loops).
+# 4. Rewrite the each() method that prints all students using while or until control flow methods (Loops).
+# 5. Add more information for each student: hobbies, country of birth, height, etc.
+#    This looks like it should be combined with exercise 7.
+# 7. In the input_students method the cohort value is hard-coded.
+#    How can you ask for both the name and the cohort?
+#    What if one of the values is empty? Can you supply a default value?
+#    The input will be given to you as a string. How will you convert it to a symbol?
+#    What if the user makes a typo?
 
+def confirm_input(variable, input)
+  while !(input.empty?) do
+    puts "You typed #{variable}."
+    puts "If you are happy with your input, type enter. Otherwise, type your input again."
+    input = gets.chomp
+    if input.empty?
+    else
+      variable = input
+    end
+  end
+  variable
+end
 def input_students
-  puts "Please enter the names of the students."
-  puts "To finish, just hit return twice."
-  # Create an empty array
+  puts "Please enter the name of a student, or type stop to finish."
   students = []
-  # Get the first name
   name = gets.chomp
-  # While the name is not empty, repeat this code
-  while !name.empty? do
-    # Add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    # Get another name from the user
+  input = name
+  name = confirm_input(name, input)
+
+  while !(name == "stop") do
+    name = "name unknown" if name.empty?
+    puts "Please enter the cohort of that student."
+    cohort = gets.chomp
+    cohort = "unknown" if cohort.empty?
+    puts "Please enter that student's country of birth"
+    country_of_birth = gets.chomp
+    country_of_birth = "unknown" if country_of_birth.empty?
+    puts "Please enter the hobbies of that student, separated by a comma and a space"
+    hobbies = gets.chomp
+    hobbies = "unknown" if hobbies.empty?
+    puts "Please enter the height of that student"
+    height = gets.chomp
+    height = "unknown" if height.empty?
+    students << {
+      name: name.to_sym,
+      cohort: cohort.to_sym,
+      country_of_birth: country_of_birth.to_sym,
+      hobbies: hobbies.to_sym,
+      height: height.to_sym
+    }
+    puts "Now we have #{students.count} students - add another or type 'stop'"
     name = gets.chomp
   end
-  # Return the array of students
   students
 end
-
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -32,7 +65,11 @@ end
 def print_all(students)
   counter = 0
   while true do
-    puts "#{(counter + 1)}. #{students[counter][:name]} (#{students[counter][:cohort]} cohort)"
+    puts "#{(counter + 1)}. #{students[counter][:name]} "\
+         "(#{students[counter][:cohort]} cohort, "\
+         "country of birth: #{students[counter][:country_of_birth]}, "\
+         "hobbies: #{students[counter][:hobbies]}, "\
+         "height: #{students[counter][:height]})"
     counter += 1
     break if counter == students.length
   end
@@ -48,7 +85,11 @@ def print_starts_with_letter(students)
   letter = gets.chomp.downcase
   students.each_with_index do |student, index|
     if student[:name][0].downcase == letter
-      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{index + 1}. #{students[index][:name]} "\
+           "(#{students[index][:cohort]} cohort, "\
+           "country of birth: #{students[index][:country_of_birth]}, "\
+           "hobbies: #{students[index][:hobbies]}, "\
+           "height: #{students[index][:height]})"
     end
   end
 end
@@ -59,7 +100,11 @@ def print_names_of_length_less_than_n(students)
   name_length = gets.chomp.downcase.to_i
   students.each_with_index do |student, index|
     if student[:name].length < name_length
-      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{index + 1}. #{students[index][:name]} "\
+           "(#{students[index][:cohort]} cohort, "\
+           "country of birth: #{students[index][:country_of_birth]}, "\
+           "hobbies: #{students[index][:hobbies]}, "\
+           "height: #{students[index][:height]})"
     end
   end
 end
